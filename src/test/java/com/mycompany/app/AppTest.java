a0e266b69d702277f5653086a7e3afac4394ceff
KSEUNGJ <dldzmwn@gmail.com>
2024-07-02 13:53:27 +0900

    Update AppTest.java

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -20,6 +20,6 @@ public class AppTest
     public void testAppMessage()
     {
         App app = new App();
-        assertEquals("HI CITY", app.getMessage());
+        assertEquals("Hello World!", app.getMessage());
     }
 }
